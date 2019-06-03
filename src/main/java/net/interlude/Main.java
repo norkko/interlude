11a648bda0600a647971d1139e94bb08ae563c1c
agge <boundlessvolatilecrow@gmail.com>
2019-06-03 21:57:49 +0200

    init

---
+++ src/main/java/net/interlude/Main.java
@@ -0,0 +1,30 @@
+package net.interlude;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    @RequestMapping("/")
+    public String index() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        logger.info("Main started");
+    }
+}
