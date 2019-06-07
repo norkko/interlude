891382c411f2bc45feca911e65cbd8a85c12e868
agge <boundlessvolatilecrow@gmail.com>
2019-06-07 04:23:38 +0200

    add start script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf ./eureka/target ./service/target
+mvn package
+if [[ "$?" -ne 0 ]] ; then
+  echo 'mvn package failed'; exit $rc
+else
+  if [ "$1" ]; then 
+  	yes Y |docker-compose rm
+    docker-compose up --build   
+  else 
+  	docker-compose up
+  fi
+fi
