e276f0ab4839570f6243ff5814e6a348db35acb2
agge <boundlessvolatilecrow@gmail.com>
2019-06-10 16:46:15 +0200

    refactor auth service

---
+++ auth/src/main/java/net/interlude/auth/service/AuthService.java
@@ -0,0 +1,8 @@
+package net.interlude.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthService {
+
+}
---
+++ auth/src/main/java/net/interlude/auth/service/AuthServiceImpl.java
@@ -0,0 +1,8 @@
+package net.interlude.auth.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthServiceImpl implements AuthService {
+
+}
