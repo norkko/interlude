d8d1c7035911c934e10b5f6405e6875d40b15ba5
agge <boundlessvolatilecrow@gmail.com>
2019-06-13 22:16:15 +0200

    add basic zuul filter

--- zuul/src/main/java/net/interlude/zuul/ZuulApplication.java
+++ zuul/src/main/java/net/interlude/zuul/ZuulApplication.java
@@ -1,5 +1,6 @@
 package net.interlude.zuul;
 
+import net.interlude.zuul.filter.CustomZuulFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -17,6 +19,11 @@ public class ZuulApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@Bean
+	public CustomZuulFilter customZuulFilter() {
+		return new CustomZuulFilter();
 	}
 
 	@Override
---
+++ zuul/src/main/java/net/interlude/zuul/filter/CustomZuulFilter.java
@@ -0,0 +1,36 @@
+package net.interlude.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CustomZuulFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        logger.info(request.getMethod() + " requesting " + request.getRequestURL().toString());
+        return null;
+    }
+}
