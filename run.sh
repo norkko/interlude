3ed74ba57227e97aae0d2b140e844e214ffa044f
august-norkko <boundlessvolatilecrow@gmail.com>
2019-12-07 16:37:19 +0100

    update run script

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-mvn package
+
+for d in *; do
+	if [ -d ${d} ]; then
+		echo removing target from $d
+		rm -rf -f ./target
+	fi
+done
+
+./mvnw package
 if [[ "$?" -ne 0 ]] ; then
-  echo 'mvn package failed'; exit $rc
+  echo 'mvn package failed'
+  exit $rc
 else
   if [ "$1" ]; then
   	yes Y |docker-compose rm
